8600fb6474a846ff46c235f24b0628bafb3271cf
Tarcisio Andrade <tarcisiof.andrade@hotmail.com>
2023-01-04 17:34:42 -0300

    Criando a segunda pagina (cardápio)

---
+++ Cardapio/JsonPizza.js
@@ -0,0 +1,12 @@
+let pizzaJson = [
+    {id:1, sizes:['320g', '860g']},
+    {id:2, sizes:['320g', '860g']},
+    {id:3, sizes:['320g', '860g']},
+    {id:4, sizes:['320g', '860g']},
+    {id:5, sizes:['320g', '860g']},
+    {id:6, sizes:['320g', '860g']},
+    {id:7, sizes:['320g', '860g']},
+    {id:8, sizes:['320g', '860g']},
+    {id:9, sizes:['320g', '860g']},
+    {id:10, sizes:['320g', '860g']},
+]
